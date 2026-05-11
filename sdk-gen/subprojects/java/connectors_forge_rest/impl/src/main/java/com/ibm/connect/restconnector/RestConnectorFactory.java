b8d4dd089cade4c07195eab8fabd5c7ecac18f01
marek-zuwala <marek.zuwala@ibm.com>
2026-05-11 17:31:04 +0200

    repairs after rebase
    Signed-off-by: marek-zuwala <marek.zuwala@ibm.com>

--- sdk-gen/subprojects/java/connectors_forge_rest/impl/src/main/java/com/ibm/connect/restconnector/RestConnectorFactory.java
+++ sdk-gen/subprojects/java/connectors_forge_rest/impl/src/main/java/com/ibm/connect/restconnector/RestConnectorFactory.java
@@ -91,14 +91,6 @@ public class RestConnectorFactory extends PooledConnectorFactory
                 LOGGER.info("Loaded REST connector '{}' from file: {}", connectorName, configFile.getName());
             } catch (java.io.IOException e) {
                 LOGGER.error("I/O error loading configuration from file '{}': {}", configFile.getName(), e.getMessage(), e);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-            } catch (com.fasterxml.jackson.core.JsonProcessingException e) {
-                LOGGER.error("Invalid JSON in configuration file '{}': {}", configFile.getName(), e.getMessage(), e);
->>>>>>> 472b14a (some fixes suggested by Bob review)
-=======
->>>>>>> 9ff90e5 (fix)
             } catch (IllegalArgumentException e) {
                 LOGGER.error("Invalid configuration in file '{}': {}", configFile.getName(), e.getMessage(), e);
             } catch (RuntimeException e) {
