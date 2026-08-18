1ca0491be0b646693b398cf31b3f2869bf9822a2
marek-zuwala <marek.zuwala@ibm.com>
2026-08-18 14:13:17 +0200

    fix invalid image
    Signed-off-by: marek-zuwala <marek.zuwala@ibm.com>

--- connectors-forge/codeless/deploy-container.ps1
+++ connectors-forge/codeless/deploy-container.ps1
@@ -23,7 +23,7 @@ param(
 # Configuration & Global Variables
 # ============================================
 
-$Image = "ghcr.io/marek-zuwala/connectors-forge:1.0.6.3"
+$Image = "ghcr.io/marek-zuwala/connectors-forge:1.0.6.4"
 $ContainerId = $null
 $ContainerName = $null
 $TempDir = $null
