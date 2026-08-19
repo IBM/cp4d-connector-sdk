8e6d50e8629c472f85d39d13c9f98d3d202bccef
marek-zuwala <marek.zuwala@ibm.com>
2026-08-19 16:57:18 +0200

    final version bump
    Signed-off-by: marek-zuwala <marek.zuwala@ibm.com>

--- connectors-forge/codeless/deploy-container.ps1
+++ connectors-forge/codeless/deploy-container.ps1
@@ -23,7 +23,7 @@ param(
 # Configuration & Global Variables
 # ============================================
 
-$Image = "ghcr.io/marek-zuwala/connectors-forge:1.0.6.10"
+$Image = "ghcr.io/marek-zuwala/connectors-forge:1.0.6.11"
 $ContainerId = $null
 $ContainerName = $null
 $TempDir = $null
